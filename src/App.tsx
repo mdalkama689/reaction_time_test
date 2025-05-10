f3ac13a6f0dfdae2c819dbcc2390f982420bd03f
mdalkama <mdalkama689@gmail.com>
2025-05-10 10:59:44 +0530

    add info icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import ReactionGame from './pages/ReactionGame';
 import Results from './pages/Results';
+import { useEffect, useState } from 'react';
+import MobileShare from './components/share/MobileShare';
+import TabShare from './components/share/TabShare';
 
 function App() {
+    const [deviceType, setDeviceType] = useState<"big" | "small">("big");
+
+
+  useEffect(() => {
+    const handleResize = () => {
+        setDeviceType(window.innerWidth < 1024 ? "small" : "big");
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
   return (
     <BrowserRouter>
+        {deviceType === "small" ? <MobileShare /> : <TabShare />}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
